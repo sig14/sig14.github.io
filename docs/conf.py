d5ab5346c2b47e11eb5f5845d4c71cc5d9c44b3c
cminot <corentinminot90@gmail.com>
2022-06-03 11:51:58 +0200

    test js

--- docs/conf.py
+++ docs/conf.py
@@ -75,6 +75,8 @@ pygments_style = None
 
 # -- Options for HTML output -------------------------------------------------
 
+html_js_files = ['script.js']
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
