a86d4ce1c47fee485552d9df55deb159d09efa0e
Theogron <97178883+Theogron@users.noreply.github.com>
2023-02-27 20:36:32 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -61,7 +61,7 @@ master_doc = 'index'
 html_context = {'display_github': True,
                 'github_user': 'sig14',
                 'github_repo': ' sig14.github.io',
-                'github_version': 'master/',
+                'github_version': 'main/',
                }
 
 # https://github.com/sphinx-contrib/emojicodes/pull/22/files
